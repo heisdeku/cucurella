e012bc425e02e1030c6a5b93b702ce6c82dd1195
Deku || Feranmi <adedokunoluwaferanmi@gmail.com>
2023-08-03 00:03:25 +0100

    feat: setup for ios and installed packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@libs': './src/libs',
+          '@screens': './src/screens',
+          '@stacks': './src/stacks',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
