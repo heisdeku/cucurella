51954e70006c592e3c349f81a4e0b20a231a9082
Deku || Feranmi <adedokunoluwaferanmi@gmail.com>
2023-08-10 05:01:35 +0100

    chore: more setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {ThemeProvider} from 'styled-components/native';
+import theme from '@libs/theme';
 
 function App(): JSX.Element {
   return (
     <GestureHandlerRootView>
-      <NavigationContainer>
-        <SafeAreaView>
-          <StatusBar barStyle={'dark-content'} />
-          <ScrollView contentInsetAdjustmentBehavior="automatic">
-            <Text>Welcome to Ofayd</Text>
-          </ScrollView>
-        </SafeAreaView>
-      </NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <SafeAreaView>
+            <StatusBar barStyle={'dark-content'} />
+            <ScrollView contentInsetAdjustmentBehavior="automatic">
+              <Text>Welcome to Ofayd</Text>
+            </ScrollView>
+          </SafeAreaView>
+        </NavigationContainer>
+      </ThemeProvider>
     </GestureHandlerRootView>
   );
 }
---
+++ src/libs/theme.ts
@@ -0,0 +1,24 @@
+export default {
+  fonts: {
+    300: 'HelveticaNeueThin',
+    400: 'HelveticaNeue',
+    500: 'HelveticaNeueMedium',
+    600: 'HelveticaNeueBold',
+    700: 'HelveticaNeueBold',
+    800: 'HelveticaNeueBlack',
+    900: 'HelveticaNeueHeavy',
+  },
+  colors: {
+    white: '#FFFFFF',
+    white100: '#ffffffb3',
+    white200: '#ffffff4d',
+    white300: '#ffffff99',
+    grey200: '#3C3C3C',
+    black200: '#0B0B0B',
+    black300: '#1E1E1E',
+    black400: '#151515',
+    purple: '#6128D0',
+    alertYellow: '#FFD700',
+    alertRed: '#C22323',
+  },
+};
